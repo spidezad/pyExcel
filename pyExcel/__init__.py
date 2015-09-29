db8e11f918a9b75887b10895bc534c541c7cee0e
Tan Kok Hua <spider123@gmail.com>
2015-09-29 22:24:10 +0800

    Change format for pip
    enable setup for pip install -- underconstruction

---
+++ pyExcel/__init__.py
@@ -0,0 +1,1 @@
+from .pyExcel import pyExcel
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pyExcel',
+      version='0.1',
+      description='Python API to Excel ',
+      author='Tan Kok Hua',
+      author_email='kokhua81@gmail.com',
+      packages=['pyExcel'],
+      zip_safe=False)
